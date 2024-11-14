b103fd1d0694e3e22847ae724075ec87adbebf8a
Alex Nico <a.nicolaidis@outlook.com>
2024-11-14 18:48:51 +1000

    feat: Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CitySearchComponent } from './city-search/city-search.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CitySearchComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'city-weather';
