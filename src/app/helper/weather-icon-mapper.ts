8d02933267fa10f896cdfd13bcfdf9645e70eb15
Alex Nico <a.nicolaidis@outlook.com>
2024-12-01 05:48:18 +1000

    feat: add icon mapper

---
+++ src/app/helper/weather-icon-mapper.ts
@@ -0,0 +1,4 @@
+export function mapIconCodeToImageURL(iconCode: string): string {
+  //link to open weather docs https://openweathermap.org/weather-conditions
+  return `https://openweathermap.org/img/wn/${iconCode}@2x.png`;
+}
